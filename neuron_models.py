e82ebc95d8f99b5c4c785a01b5452f86a5d3bb7e
Jason <Jason@Jasons-MacBook-Pro-5.local>
2019-04-30 12:46:20 -0700

    fixed bug in STDP

--- neuron_models.py
+++ neuron_models.py
@@ -343,6 +343,8 @@ class s_lifSTDP_ex:
         self.g_syn = conduct
         self.dApre = eta*conduct
         self.dApost = -eta*conduct * taupre / taupost * 1.05
+        self.taupre = taupre
+        self.taupost = taupost
         return
 
     def eqs(self):
@@ -367,7 +369,9 @@ class s_lifSTDP_ex:
     def namespace(self):
         return dict(g_syn = self.g_syn,
                     dApre = self.dApre,
-                    dApost = self.dApost)
+                    dApost = self.dApost,
+                    taupre = self.taupre,
+                    taupost = self.taupost)
 
     def init_cond(self):
         return dict(w_syn = 'rand()*g_syn')
